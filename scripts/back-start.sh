e03bd5a9a2515804329dd2569e0986ef770c61cb
Sihyeon0123 <snc100194@gmail.com>
2025-12-18 16:38:45 +0900

    백엔드 스크립트

--- scripts/back-start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/back
-java -Xms512m -Xmx1024m -jar app.jar
--- scripts/front-start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/front
-npm install
-npm run build
-pm2 restart next || pm2 start npm --name next -- start
